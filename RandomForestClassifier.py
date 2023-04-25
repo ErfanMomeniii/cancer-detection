ff24d3ba6390eacbf623085213a2cd3db199875d
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-04-25 23:52:33 +0430

    feat: add some logics

---
+++ RandomForestClassifier.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+data = load_breast_cancer()
+
+X = data['data']
+Y = data['target']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+clf = RandomForestClassifier()
+clf.fit(X_train, Y_train)
+
+print(clf.score(X_test, Y_test))
